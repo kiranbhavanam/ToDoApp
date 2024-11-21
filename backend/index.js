9a11e85de4c753ddb5d6144e79437aa15da873ed
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-11-21 23:23:06 +0530

    Integrated fE with BE

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express=require("express")
 const bodyParser=require("body-parser")
 const app=express();
 app.use(bodyParser.json())
+const cors = require("cors");
+app.use(cors());
+
 const todos=[]
 let id=1;
 app.get("/todos",(req,res)=>{
@@ -21,7 +24,8 @@ app.post("/todos",(req,res)=>{
     return res.status(201).send(newTodo)
 })
 app.put("todos/:id",(req,res)=>{
-    const target=todos.find(todo=>todo.id===parseInt(req.body.id))
+
+    const target=todos.find(todo=>todo.id===parseInt(req.params.id))
     if(!target)
         return res.status(400).send("No such task")
     target.completed=true;
